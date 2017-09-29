4fcc24d6b7396744e6ad51ec3299e68048b5b31c
Simon <simon.haefeli@epfl.ch>
2017-09-29 17:48:08 +0200

    Initial commit: Dummy middleware class and java main class

---
+++ src/ch/ethz/asltest/MyMiddleware.java
@@ -0,0 +1,16 @@
+package ch.ethz.asltest;
+
+import java.util.List;
+
+/**
+ * Created by simon on 29.09.17.
+ */
+public class MyMiddleware implements Runnable{
+
+    public MyMiddleware(String ip, int port, List<String> mcAddresses, int numThreadsPTP, boolean readSharded){
+
+    }
+    public void run(){
+
+    }
+}
